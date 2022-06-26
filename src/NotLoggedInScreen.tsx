43defaed112753b6ef75aaa087d839d4a981f233
Adrian Miska <adrian.miska97@gmail.com>
2022-06-27 00:53:35 +0200

    Add Login button

--- src/NotLoggedInScreen.tsx
+++ src/NotLoggedInScreen.tsx
@@ -3,8 +3,12 @@ import React from "react";
 export class NotLoggedInScreen extends React.Component {
     render() {
         return <div>
-            <button>
-                Login to Apple Music
+            <button className="bg-slate-500 hover:bg-slate-700
+                text-white font-bold py-2 px-4 rounded"
+                onClick={() =>  {
+                    window.MusicKit.getInstance().authorize();
+                }
+                }>Login with Apple Music
             </button>
         </div>;
     }
