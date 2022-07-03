a4feb0611e9c3a535bca4d31313864b44c382642
Adrian Miska <adrian.miska97@gmail.com>
2022-07-04 00:51:14 +0200

    Fix sort for undefined ratings

--- src/routes/SongRating.tsx
+++ src/routes/SongRating.tsx
@@ -81,19 +81,19 @@ function PlaylistSong(props: { song: MusicKit.Songs | MusicKit.MusicVideos, rati
  */
 function PlaylistElo(props: { playlistId: string, songs: (MusicKit.Songs | MusicKit.MusicVideos)[], ratings: { [key: string]: number } }) {
 
-    function sortedSongs() {
-        return props.songs.sort((a, b) => {
-            let aRating = props.ratings[a.id];
-            let bRating = props.ratings[b.id];
-            if (aRating === bRating) {
-                return 0;
-            }
-            return aRating > bRating ? -1 : 1;
+    let [sortedSongs, setSortedSongs] = React.useState<(MusicKit.Songs | MusicKit.MusicVideos)[]>([]);
+
+    useEffect(() => {
+        let sorted = props.songs.sort((a, b) => {
+            let aRating = props.ratings[a.id] || 0;
+            let bRating = props.ratings[b.id] || 0;
+            return bRating - aRating;
         });
-    }
+        setSortedSongs(sorted);
+    }, [props.songs, props.ratings]);
 
     return <div>
-        {sortedSongs().map((song: Songs | MusicVideos) => {
+        {sortedSongs.map((song: Songs | MusicVideos) => {
             let rating = props.ratings[song.id] || 0;
             return <PlaylistSong key={song.id} song={song} rating={rating}/>
         })}
