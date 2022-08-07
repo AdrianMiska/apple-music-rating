56b43809b49136d882c945760741c2b93cc7d302
Adrian Miska <adrian.miska97@gmail.com>
2022-08-07 21:37:44 +0200

    Remove direct display of rating to prevent biases

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,23 +1,11 @@
-import {getEloRating} from "../EloUtils";
-import React, {useEffect} from "react";
+import React from "react";
 import {Artwork} from "./Artwork";
 import {MusicWrapper} from "../MusicWrapper";
 
 /**
- * Displays a song with its album art, title, artist and current rating.
- *
+ * Displays a song with its album art, title and artist.
  */
 export function Song(props: { song: MusicWrapper.Song, playlistId: string }) {
-
-    let [rating, setRating] = React.useState<number>(0);
-
-
-    useEffect(() => {
-        getEloRating(props.playlistId, props.song).then(rating => {
-            setRating(rating);
-        });
-    }, [props.song, props.playlistId]);
-
 
     return <div className="flex flex-col items-center px-4">
         <Artwork artwork={props.song.artwork || null}/>
@@ -25,7 +13,6 @@ export function Song(props: { song: MusicWrapper.Song, playlistId: string }) {
         <div className="text-center">
             <h1 className="text-xl font-bold my-2">{props.song.title}</h1>
             <h2 className="text-sm font-semibold mb-2">{props.song.artist}</h2>
-            <h2 className="text-sm font-semibold mb-2">Elo: {rating.toFixed(1)}</h2>
         </div>
     </div>;
 
